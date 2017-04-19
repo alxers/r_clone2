1c18042030c4098c39c38a3fcad28d2fae58d82f
Alexey Ershov <alex4rom@gmail.com>
2017-04-19 11:48:56 +0300

    Create user with jwt

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,22 @@
-class UsersController < ApiResourceController
-  def index
-  end
+module Api
+  module V1
+    class UsersController < Api::V1::ApiResourceController
+      def index
+      end
 
-  def create
-    respond_with User.create(auth_token: params[:auth_token], email: params[:email], password: password[:password])
-  end
+      def create
+        user = User.create!(email: user_params[:email],
+                            password: user_params[:password]
+                            )
+        jwt = JsonWebToken.encode(user: user.id)
+        render json: { user: user, auth_token: jwt }
+      end
 
-  private
+      private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+      end
+    end
   end
 end
