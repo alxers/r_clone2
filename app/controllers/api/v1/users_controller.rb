332aebd8b786a8d54e63193d84cd92cd66459afa
Alexey Ershov <alex4rom@gmail.com>
2017-04-24 17:49:48 +0300

    Comment out verification

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     class UsersController < ApplicationController
       skip_before_action :authenticate_user!, only: :create
-      skip_before_action :verify_authenticity_token
     end
   end
 end
